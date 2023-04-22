abffc7e929f425902b1eec75230fc178c99a3767
sandeepkdasari143 <sandeepkumar25820@gmail.com>
2023-04-22 19:42:58 +0530

    Reviewed

--- config/db.connector.js
+++ config/db.connector.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const mongoose = require("mongoose");
 
 const connectToMongoDB = () => {
-    mongoose.connect(process.env.MONGO_URL, {
+    mongoose.connect(process.env.MONGO_URI, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
     })
