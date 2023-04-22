3cc26bbc188adabdff7cb75303086f80be18463f
sandeepkdasari143 <sandeepkumar25820@gmail.com>
2023-04-22 15:35:07 +0530

    Cloudinary Removed

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ const connectToMongoDB = require('./config/db.connector');
 connectToMongoDB();
 
 //Config the Cloudinary Service
-cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
-    secure: true //https-secured-urls-flag
-})
+// cloudinary.config({
+//     cloud_name: process.env.CLOUDINARY_NAME,
+//     api_key: process.env.CLOUDINARY_API_KEY,
+//     api_secret: process.env.CLOUDINARY_API_SECRET,
+//     secure: true //https-secured-urls-flag
+// })
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running at port: ${process.env.PORT}`);
